2740c0cc1ebb2693af8cfd1a6d5988d248b2ee5c
cri698 <79714014+cri698@users.noreply.github.com>
2024-09-27 11:39:51 -0400

    padding login fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,10 +23,10 @@ const points = [
 document.getElementById("response").innerHTML=points;
 
 function myFunction(){
-    points.sort(function(a,b){return 0.5 - Math.random()});
+    points.sort(function(a,b){return 0.5 - Math.random()}); 
     document.getElementById("response").innerHTML=points[0];
     document.getElementById("response").style.fontSize="18px";
-    setTimeout(timeup, 4000);
+    setTimeout(timeup, 4000); // 4 sec 
 
     function timeup(){
         document.getElementById("response").innerHTML="8";
